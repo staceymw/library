f0857c1b81470f950b7ddbc33d79f1dace28d0f3
staceymw <116839832+staceymw@users.noreply.github.com>
2023-03-16 20:12:41 -0400

    Add event listener to add book button

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
 let myLibrary = [];
 
 class Book {
-    constructor(title, author, pageCount, status) {
-        this.title = title
-        this.author = author
-        this.pageCount = pageCount
-        this.status = status
+    constructor(title, author, pageCount, read) {
+        this.title = title;
+        this.author = author;
+        this.pageCount = pageCount;
+        this.read = read;
     }
 }
 
-function addBookToLibrary(title, author, pageCount, status) {
-    const newBook = new Book(title, author, pageCount, status);
-    myLibrary.push(newBook);
+let title = document.getElementById("book-title").value;
+let author = document.getElementById("book-author").value;
+let pageCount = document.getElementById("page-count").value;
+let read = document.getElementById("read-checkbox").checked;
+
+function addBookToLibrary(title, author, pageCount, read) {
+
+    const newBook = new Book(title, author, pageCount, read);
+    console.log(newBook)
+    //myLibrary.push(newBook);
     //displayBook();
 }
 
+document.getElementById("add-book").addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("Hello World");
+})
+
 function displayBook() {
-    
+
 }
