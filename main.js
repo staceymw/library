1a7e22eb204535a4c65afd62a2dce5be379c4140
staceymw <116839832+staceymw@users.noreply.github.com>
2023-03-16 20:53:02 -0400

    add eslint config

--- main.js
+++ main.js
@@ -1,32 +1,35 @@
 let myLibrary = [];
 
 class Book {
-    constructor(title, author, pageCount, read) {
-        this.title = title;
-        this.author = author;
-        this.pageCount = pageCount;
-        this.read = read;
-    }
+  constructor(title, author, pageCount, read) {
+    this.title = title;
+    this.author = author;
+    this.pageCount = pageCount;
+    this.read = read;
+  }
 }
 
-let title = document.getElementById("book-title").value;
+// let title = document.getElementById("book-title").value;
 let author = document.getElementById("book-author").value;
 let pageCount = document.getElementById("page-count").value;
 let read = document.getElementById("read-checkbox").checked;
 
 function addBookToLibrary(title, author, pageCount, read) {
-
-    const newBook = new Book(title, author, pageCount, read);
-    console.log(newBook)
-    //myLibrary.push(newBook);
-    //displayBook();
+  const newBook = new Book(title, author, pageCount, read);
+  console.log(newBook);
+  //myLibrary.push(newBook);
+  //displayBook();
 }
 
-document.getElementById("add-book").addEventListener("click", function(event) {
-    event.preventDefault();
-    console.log("Hello World");
-})
+document
+  .getElementById("new-book-form")
+  .addEventListener("submit", handleAddBook);
 
-function displayBook() {
-
+function handleAddBook(event) {
+  event.preventDefault();
+  console.log(event.target[0]);
+  const title = event.target[0];
+  console.log("Hello World");
 }
+
+function displayBook() {}
