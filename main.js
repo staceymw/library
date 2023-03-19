87be318a565eb25422a717481762a90d706ddf71
staceymw <116839832+staceymw@users.noreply.github.com>
2023-03-19 14:40:29 -0400

    Refactor and add delete button

--- main.js
+++ main.js
@@ -6,61 +6,128 @@ class Book {
     this.author = author;
     this.pageCount = pageCount;
     this.readStatus = readStatus;
+    this.index;
   }
 }
 
-function addBookToLibrary(event) {
+function getNewCard() {
+  const newCard = document.createElement("div");
+
+  newCard.classList.add("card");
+  newCard.setAttribute("id", "card");
+
+  return newCard;
+}
+
+function getNewTitle(title) {
+  const newTitle = document.createElement("h2");
+
+  newTitle.setAttribute("id", "card-heading");
+  newTitle.textContent = title;
+
+  return newTitle;
+}
+
+function getNewAuthor(author) {
+  const newAuthor = document.createElement("p");
+  newAuthor.textContent = `Written by: ${author}`;
+
+  return newAuthor;
+}
+
+function getNewPageCount(pageCount) {
+  const newPageCount = document.createElement("p");
+  newPageCount.textContent = `${pageCount} pages`;
+
+  return newPageCount;
+}
+
+function getReadStatusButton() {
+  const readStatusButton = document.createElement("button");
+  const checkbox = document.getElementById("form-read-status");
+
+  if (checkbox.checked === true) {
+    readStatusButton.textContent = "Read";
+    readStatusButton.classList.add("read");
+  } else {
+    readStatusButton.textContent = "Unread";
+    readStatusButton.classList.add("unread");
+  }
+
+  return readStatusButton;
+}
+
+function handleReadButton(event) {
+  const button = event.target;
+
+  if (button.classList[0] === "read") {
+    button.classList.remove("read");
+    button.classList.add("unread");
+    button.textContent = "Unread";
+  } else {
+    button.classList.remove("unread");
+    button.classList.add("read");
+    button.textContent = "Read";
+  }
+}
+
+function constructDeleteButton(index) {
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.addEventListener("click", (e) => {
+    myLibrary.splice(index, 1);
+    e.target.parentElement.remove();
+  });
+
+  return deleteButton;
+}
+
+function addToLibrary(event) {
   const title = event.target[0].value;
   const author = event.target[1].value;
   const pageCount = event.target[2].value;
-  const readStatus = document.getElementById("read-status").checked;
+  const readStatus = document.getElementById("form-read-status").checked;
   const newBook = new Book(title, author, pageCount, readStatus);
+
   myLibrary.push(newBook);
 }
 
-function displayBook(event) {
+function constructBook(book, index) {
+  const { title, author, pageCount } = book;
   const books = document.getElementById("books");
-  const newCard = document.createElement("div");
-  newCard.classList.add("card");
-  newCard.setAttribute("id", "card");
-  
-  const newTitle = document.createElement("h2");
-  const title = event.target[0].value;
-  newTitle.setAttribute("id", "card-heading");
-  newTitle.textContent = title;
+
+  const newCard = getNewCard();
+
+  if (book.index >= 0) {
+    return;
+  }
+
+  const newTitle = getNewTitle(title);
+  const newAuthor = getNewAuthor(author);
+  const newPageCount = getNewPageCount(pageCount);
+  const readStatusButton = getReadStatusButton();
+  const deleteButton = constructDeleteButton(index);
+
   newCard.append(newTitle);
-
-  const newAuthor = document.createElement("p");
-  const author = event.target[1].value;
-  newAuthor.textContent = `Written by: ${author}`;
   newCard.append(newAuthor);
-
-  const newPageCount = document.createElement("p");
-  const pageCount = event.target[2].value;
-  newPageCount.textContent = `${pageCount} pages`;
   newCard.append(newPageCount);
+  newCard.append(readStatusButton);
+  readStatusButton.addEventListener("click", handleReadButton);
+  newCard.append(deleteButton);
+  newCard.setAttribute("data-index", index);
 
-  const label = document.createElement("label");
-  label.htmlFor = "read-status";
-  label.textContent = "Check if read.";
-  label.setAttribute("id", "read-status-label");
-
-  const newReadStatus = document.createElement("input");
-  newReadStatus.setAttribute("type", "checkbox");
-  newReadStatus.setAttribute("id", "read-status");
-  newCard.append(label);
-  newCard.append(newReadStatus);
+  book.index = index;
   books.append(newCard);
-  //display is done
 }
-
 
 document.getElementById("new-book-form").addEventListener("submit", (e) => {
   e.preventDefault();
-  displayBook(e);
-  addBookToLibrary(e);
+  addToLibrary(e);
+
+  myLibrary.forEach((book, index) => {
+    constructBook(book, index);
+  });
+
+  console.log(myLibrary);
   document.getElementById("new-book-form").reset();
 });
-
-  
-  
